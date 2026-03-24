671228d0de0e25a018068a553354e607aae5b8e1
Ra2den <cgerlach2003@gmail.com>
2026-03-24 17:45:46 +0100

    added first proof of concept

---
+++ src/main/java/com/example/restapimitspringaimcp/MyController.java
@@ -0,0 +1,25 @@
+package com.example.restapimitspringaimcp;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+class MyController {
+
+    private final ChatClient chatClient;
+
+    public MyController(ChatClient.Builder chatClientBuilder) {
+        this.chatClient = chatClientBuilder.build();
+    }
+
+    @GetMapping("/ai")
+    String generation(String userInput) {
+        return this.chatClient.prompt()
+                .user(userInput)
+                .call()
+                .content();
+    }
+}
