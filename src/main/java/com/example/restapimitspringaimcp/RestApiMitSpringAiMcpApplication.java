caacea622b0fed477e8f5628bd15f72a76a3041a
Ra2den <cgerlach2003@gmail.com>
2026-03-24 16:56:50 +0100

    Initial commit

---
+++ src/main/java/com/example/restapimitspringaimcp/RestApiMitSpringAiMcpApplication.java
@@ -0,0 +1,13 @@
+package com.example.restapimitspringaimcp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiMitSpringAiMcpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestApiMitSpringAiMcpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/restapimitspringaimcp/RestApiMitSpringAiMcpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.restapimitspringaimcp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiMitSpringAiMcpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
